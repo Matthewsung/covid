39c6ccb3f8c1282a9dd2691d6d599ad6e5906dd5
juyeongsung <jooyongs1@gmail.com>
2021-07-28 15:20:57 +0900

    7.28

--- src/component/Head.js
+++ src/component/Head.js
@@ -4,7 +4,7 @@ import styles from './Head.module.css'
 const Head = () => {
   return(
     <div className={styles.header}>
-      <Link to='/'><h1>COVID-19 상황</h1></Link>
+      <Link to='/covid'><h1>COVID-19 상황</h1></Link>
       <select>
         <option>국내</option>
         <option>국외</option>
